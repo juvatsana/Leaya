9be8a849c3d3726f12ff2e077403f657da10559e
juvatsana <julienvorabout@hotmail.fr>
2023-12-28 02:21:41 +0100

    Merge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from 'next/app'
 import { Raleway } from 'next/font/google'
+import 'tailwindcss/tailwind.css'
 
 // If loading a variable font, you don't need to specify the font weight
 const raleway = Raleway({ subsets: ['latin'] })
- 
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <main className={raleway.className} >
-            <Component {...pageProps} />
-        </main>
-    )
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
