b2f24a8e651009d9b6332449774b6e0a73833779
juvatsana <julienvorabout@hotmail.fr>
2024-01-03 20:54:56 +0100

    Fix menu + ajout theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
+import Navbar from '@/components/NavBar/NavBar'
+import { ThemeProvider, createTheme } from '@mui/material'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
-import Navbar from '@/components/NavBar/NavBar'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-
   const heroSectionStyle = {
     backgroundImage: "url('/assets/background.svg')",
     backgroundRepeat: 'no-repeat',
@@ -11,10 +11,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     backgroundSize: '155% auto',
   }
 
+  const theme = createTheme({
+    components: {
+      MuiList: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#B18F40',
+            color: 'white',
+          },
+        },
+      },
+    },
+    // Ajoutez d'autres configurations de thème ici
+  })
+
   return (
     <main style={heroSectionStyle} className="min-h-screen overflow-hidden">
-      <Navbar></Navbar>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Navbar></Navbar>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </main>
   )
 }
