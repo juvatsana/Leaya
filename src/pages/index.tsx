2c21e6159cec74004a0e9c75bcacaa1387bb5390
Jeremy Sananikone <jeremy.snnk@gmail.com>
2024-01-08 13:57:09 +0100

    fix responsive bug in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,47 +12,40 @@ const heroSectionStyle = {
 
 export default function App() {
   const theme = useTheme()
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-  const styleTitle: CSSProperties = isSmallScreen
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down('md'))
+  const styleTitle: CSSProperties = isMediumScreen
     ? {
         position: 'relative',
-        color: 'white',
-        fontSize: '40px',
-        fontWeight: '600',
-        paddingLeft: '2rem',
-        margin: '1rem',
-        lineHeight: '3.5rem',
-      }
-    : {
-        position: 'relative',
-        top: '5rem',
-        left: '5rem',
         color: 'white',
         fontSize: '48px',
         fontWeight: '600',
-        padding: '3rem',
+        lineHeight: '3.5rem',
+      }
+    : {
+        position: 'relative',
+        color: 'white',
+        fontSize: '48px',
+        fontWeight: '600',
         lineHeight: '3.5rem',
       }
 
-  const styleDownload = isSmallScreen ? 'pl-8' : 'relative left-32'
 
-  const styleImage: CSSProperties = isSmallScreen
-    ? {
-        height: '65vh',
-        position: 'relative',
-        width: '60vw',
-        top: '4rem',
-      }
-    : {
-        width: '300px',
-        height: '80vh',
-        position: 'relative',
-      }
+  // const styleImage: CSSProperties = isMediumScreen
+  //   ? {
+  //       width: '458px',
+  //       height: 'auto',
+  //       position: 'relative',
+  //     }
+  //   : {
+  //       width: '458px',
+  //       height: 'auto',
+  //       position: 'relative',
+  //     }
 
   return (
-    <Grid container style={{ justifyContent: 'center' }} className="text-2xl">
-      <Grid item sm={12} md={6} className="relative">
-        <h1 style={styleTitle}>
+    <Grid container style={{ justifyContent: 'center' }} className="text-2xl pt-20">
+      <Grid item sm={12} md={6} lg={7} className="flex flex-col pl-10 lg:pl-20 lg:pr-5 xl:pl-36 2xl:pl-60" >
+        <h1 style={styleTitle} className='pb-10'>
           <span
             style={{
               color: 'transparent',
@@ -68,7 +61,7 @@ export default function App() {
         <br></br>
         <br></br>
         <Image
-          className={styleDownload}
+          className='pb-10' 
           src="/assets/download_app2.png"
           alt="lien de téléchargement"
           width="400"
@@ -77,7 +70,7 @@ export default function App() {
         <br></br>
         <Link href="https://apps.apple.com/fr/app/leaya-elite/id6474127173">
           <Image
-            className={styleDownload}
+            className='pb-10' 
             src="/assets/ios.png"
             alt="lien de téléchargement"
             width="200"
@@ -87,7 +80,7 @@ export default function App() {
         <br></br>
         <Link href="https://play.google.com/store/apps/details?id=app.leayaapp.com">
           <Image
-            className={styleDownload}
+            className='pb-10' 
             src="/assets/android.png"
             alt="lien de téléchargement"
             width="200"
@@ -99,14 +92,23 @@ export default function App() {
         item
         sm={12}
         md={6}
+        lg={5}
         style={{
           display: 'flex',
           justifyContent: 'center',
           position: 'relative',
         }}
       >
-        <div style={styleImage}>
-          <Image src="/assets/phone.png" alt="phone" layout="fill" />
+        <div 
+        // style={styleImage}
+        >
+          <Image 
+          src="/assets/phone.png" 
+          alt="phone" 
+          width="458" 
+          height='914'
+          // style={{minHeight: '914px'}}
+          />
         </div>
       </Grid>
     </Grid>
